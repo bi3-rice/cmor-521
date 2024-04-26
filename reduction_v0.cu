9a0e80eae404c174aad9b7f4622658f61ca93433
bi3-rice <143285249+bi3-rice@users.noreply.github.com>
2024-04-26 14:01:47 -0500

    Update reduction_v0.cu

--- reduction_v0.cu
+++ reduction_v0.cu
@@ -31,7 +31,7 @@ __global__ void partial_reduction(const int N, float *x_reduced, const float *x)
   */
 
   int s = 1;
-  while(s < BLOCK_SIZE){
+  while(s < blockDim.x){
     if(tid % 2*s == 0){
       s_x[tid] += s_x[tid+s];
     }
